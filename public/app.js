91663c5d5b775bd3400c73c305e349c011aa1375
Jamie Sime <jamiesime@gmail.com>
2017-12-04 16:12:40 +0000

    app.js adds new cat to DOM

--- public/app.js
+++ public/app.js
@@ -1,11 +1,28 @@
 var app = function(){
 
-  addCat(newCat);
+  addCat("Cup", "Handlebars");
 
 };
 
-var addCat = function(cat){
-  var catData = document.getElementById('cat').children;
+var addCat = function(name, food){
+  var catSection = document.querySelector("#cats");
+  var cat = document.createElement("ul");
+  var catName = document.createElement("li");
+  catName.innerText = name;
+  var catFood = document.createElement("li");
+  catFood.innerText = food;
+  var catPic = document.createElement("li");
+  var catData = [];
+  catData.push(catName, catFood, catPic);
+  appendElements(catData, cat, catSection);
+};
+
+var appendElements = function(catData, cat, catSection)
+{
+  for(data of catData){
+    cat.appendChild(data);
+  }
+  catSection.appendChild(cat);
 };
 
 window.onload = app;
